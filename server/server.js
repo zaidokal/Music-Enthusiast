b576ca61a429f68902b98d7c24dfea751c98ffa2
Sufyan Khan <skhan943@uwo.ca>
2022-11-30 19:48:37 -0500

    Sorted out some directory issues

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', express.static('public'));
+app.use('/api', routes);
+
+// Define port and start server
+const port = 8080;
+app.listen(port, () => console.log(`Server running on port ${port}`)); 
