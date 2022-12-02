58587c52e5e11f402f3be6d945a45c32893c5ed2
Zaid Okal <zokal@uwo.ca>
2022-12-01 23:59:26 -0500

    Script to switch between login and register forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./views/HomePage";
@@ -6,14 +6,23 @@ import { Login } from "./Login";
 import { Register } from "./Register";
 
 const App = () => {
+  const [currentForm, setCurrentForm] = useState("login");
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
+
   return (
     // <Routes>
     //   <Route exact path="/" element={<HomePage />} />
     // </Routes>
 
     <div className="container">
-      {/* <HomePage></HomePage> */}
-      <Login></Login>
+      {currentForm === "login" ? (
+        <Login onFormSwitch={toggleForm} />
+      ) : (
+        <Register onFormSwitch={toggleForm} />
+      )}
     </div>
   );
 };
