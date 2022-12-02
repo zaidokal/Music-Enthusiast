91faefea8cd2b055f49190262b8922587641f632
Zaid Okal <zokal@uwo.ca>
2022-12-01 23:06:22 -0500

    Fixed App.js to load webpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import {Routes, Route} from 'react-router-dom';
-import './App.css';
-import HomePage from './views/HomePage';
+import React from "react";
+// import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./views/HomePage";
+import { Login } from "./Login";
+import { Register } from "./Register";
 
 const App = () => {
-  return(
-    <Routes>
-      <Route exact path='/' element={<HomePage />} />
-    </Routes>
-  )
-}
+  return (
+    // <Routes>
+    //   <Route exact path="/" element={<HomePage />} />
+    // </Routes>
+
+    <div className="container">
+      {/* <HomePage></HomePage> */}
+      <Login></Login>
+    </div>
+  );
+};
 
 export default App;
