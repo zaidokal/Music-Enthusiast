6356fbc9e987775c6d03e8cec1509b8740b74596
Zaid Okal <zokal@uwo.ca>
2022-12-01 23:57:44 -0500

    Added preventDefault and switch to register form page

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,28 +1,39 @@
 import React, { useState } from "react";
 
-export const Login = () => {
+export const Login = (props) => {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(pass);
+  };
+
   return (
-    <form>
-      <label for="emaill">email</label>
-      <input
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        type="email"
-        placeholder="email@example.com"
-      />
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="email@example.com"
+        />
 
-      <label for="password">password</label>
-      <input
-        value={pass}
-        onChange={(e) => setPass(e.target.value)}
-        type="password"
-        placeholder="********"
-      />
+        <label htmlFor="password">password</label>
+        <input
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="********"
+        />
 
-      <button>Log In</button>
-    </form>
+        <button type="submit">Log In</button>
+      </form>
+
+      <button onClick={() => props.onFormSwitch("register")}>
+        Don't have an account? Register here.
+      </button>
+    </>
   );
 };
