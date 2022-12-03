ddcdc9822826323d46f017cdc58325c329c91379
Zaid Okal <zokal@uwo.ca>
2022-12-02 22:45:28 -0500

    Added components for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,33 @@
-import GenericButton from "../components/GenericButton";
+import React from "react";
+import AccountButton from "./AccountButton";
 import { Login } from "../views/Login";
 import { Register } from "../views/Register";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
   return (
-    <div className="container">
-      <GenericButton linkTo={"/login"} text={"Login"} />
-      <GenericButton linkTo={"/register"} text={"Register"} />
-      <label>SRZ Music</label>
-    </div>
+    <>
+      <div className={styles.MainDiv}>
+        <div className={styles.CompanyDiv}>
+          <label className={styles.Company}>SRZ Music</label>
+        </div>
+
+        <div className={styles.AccountDiv}>
+          <Link>
+            <div>
+              <AccountButton
+                className={styles.Button}
+                linkTo={"/login"}
+                text={"Login"}
+              />
+              <AccountButton linkTo={"/register"} text={"Register"} />
+            </div>
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
+
+export default Header;
