d04de9eabbd8459a0bd575b10bd0f2509b7fe40f
Zaid Okal <zokal@uwo.ca>
2022-12-02 22:46:22 -0500

    Added Header to HomePage and more details

--- client/src/views/HomePage.js
+++ client/src/views/HomePage.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { Login } from "./Login";
-import { Register } from "./Register";
-import { SearchPage } from "./SearchPage";
+import { Login } from "../views/Login";
+import { Register } from "../views/Register";
+import { SearchPage } from "../views/SearchPage";
 import GenericButton from "../components/GenericButton";
+import "./HomePage.module.css";
+import Header from "../components/Header";
+// import { BackgroundPicture } from "../components/BackgroundPicture";
 
 const HomePage = () => {
   return (
     <>
-      <GenericButton linkTo={"/login"} text={"Login"} />
-      <GenericButton linkTo={"/register"} text={"Register"} />
-      <GenericButton linkTo={"/SearchPage"} text={"SearchPage"} />
+      <Header />
+
+      <GenericButton
+        linkTo={"/SearchPage"}
+        text={"Start your listening experience"}
+      />
+      <label htmlFor="LegalMessage">LISTEN TO MUSIC ANYTIME. ANYWHERE.</label>
+      <label htmlFor="LegalMessage">
+        Copyright, Privacy, and Acceptable Use
+      </label>
+
+      {/* style={{ backgroundImage: `url(${BackgroundPicture})` }} */}
     </>
   );
 };
