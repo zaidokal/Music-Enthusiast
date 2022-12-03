26954f6336a556e72bf9196f683905b06ed3fef1
Zaid Okal <zokal@uwo.ca>
2022-12-02 20:34:23 -0500

    Added searchpage to homepage

--- client/src/views/HomePage.js
+++ client/src/views/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Login } from "./Login";
 import { Register } from "./Register";
+import { SearchPage } from "./SearchPage";
 import GenericButton from "../components/GenericButton";
 
 const HomePage = () => {
@@ -8,6 +9,7 @@ const HomePage = () => {
     <>
       <GenericButton linkTo={"/login"} text={"Login"} />
       <GenericButton linkTo={"/register"} text={"Register"} />
+      <GenericButton linkTo={"/SearchPage"} text={"SearchPage"} />
     </>
   );
 };
