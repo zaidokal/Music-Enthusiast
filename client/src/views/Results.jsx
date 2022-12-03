89b29e2d1117545e1fe0abcdae82d5f89e6f16ff
Zaid Okal <zokal@uwo.ca>
2022-12-03 03:08:53 -0500

    Created results view

---
+++ client/src/views/Results.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+// import styles from "Results.module.css";
+// import PageTemplate from "../components/PageTemplate";
+
+// const Results = (props) => {
+//   return (
+//     <>
+//       <PageTemplate leftBarContent={<GenericButton />} />
+//     </>
+//   );
+// };
+
+// export default Results;
