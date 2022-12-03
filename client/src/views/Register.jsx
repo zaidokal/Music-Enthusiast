006a98b59193f96aa5ca23b6b14b99aea0f42e63
Zaid Okal <zokal@uwo.ca>
2022-12-02 20:33:23 -0500

    Removed useState from Register

--- client/src/views/Register.jsx
+++ client/src/views/Register.jsx
@@ -2,13 +2,8 @@ import React, { useState } from "react";
 import GenericButton from "../components/GenericButton";
 
 export const Register = (props) => {
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
-  const [name, setName] = useState("");
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(pass);
   };
 
   return (
@@ -16,29 +11,17 @@ export const Register = (props) => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Full Name</label>
         <input
-          value={name}
           name="name"
           id="name"
           placeholder="Full Name"
-          onChange={(e) => setName(e.target.value)}
           type="string"
         ></input>
 
         <label htmlFor="email">email</label>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="email@example.com"
-        />
+        <input type="email" placeholder="email@example.com" />
 
         <label htmlFor="password">password</label>
-        <input
-          value={pass}
-          onChange={(e) => setPass(e.target.value)}
-          type="password"
-          placeholder="********"
-        />
+        <input type="password" placeholder="********" />
 
         <button type="submit">Log In</button>
       </form>
