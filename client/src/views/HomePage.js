05973c596f15b9cea3d51d58bdc4e7b95cc3b33b
Zaid Okal <zokal@uwo.ca>
2022-12-02 20:14:14 -0500

    Added register button to homepage

--- client/src/views/HomePage.js
+++ client/src/views/HomePage.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Login } from "./Login";
+import { Register } from "./Register";
 import GenericButton from "../components/GenericButton";
 
 const HomePage = () => {
-  return <GenericButton linkTo={"/login"} text={"Lets Get Started"} />;
-  // return <div>Hello world!</div>;
+  return (
+    <>
+      <GenericButton linkTo={"/login"} text={"Login"} />
+      <GenericButton linkTo={"/register"} text={"Register"} />
+    </>
+  );
 };
 
 export default HomePage;
