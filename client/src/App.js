7622a91f0d08e72792e0d82dc0f12e11f8df52cf
Zaid Okal <zokal@uwo.ca>
2022-12-03 01:33:52 -0500

    Added results route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./views/HomePage";
 import { Login } from "./views/Login";
 import { Register } from "./views/Register";
 import { SearchPage } from "./views/SearchPage";
+import { Results } from "./views/Results";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/SearchPage" element={<SearchPage />} />
+      {/* <Route path="/Results" element={<Results />} /> */}
     </Routes>
   );
 };
