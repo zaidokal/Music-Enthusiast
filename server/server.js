24cddbf109fb137b4ff9c04207cdea4e9728970d
Sufyan Khan <skhan943@uwo.ca>
2022-12-07 15:01:39 -0500

    add headers to get around CORS policy

--- server/server.js
+++ server/server.js
@@ -11,9 +11,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 app.use("/", express.static("public"));
 app.use("/api", routes);
 
 // Define port and start server
-const port = 8080;
+const port = 8000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
