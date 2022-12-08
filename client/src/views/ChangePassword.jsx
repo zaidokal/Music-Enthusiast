3f7640f63d48136a8d90db79350507e7719a6ace
Zaid Okal <zokal@uwo.ca>
2022-12-07 20:23:15 -0500

    ChangePassword page and css

---
+++ client/src/views/ChangePassword.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import HeaderAccount from "../components/HeaderAccount";
+import styles from "./Login.module.css";
+import AccountButton from "../components/AccountButton";
+import ChangeButton from "../components/ChangeButton";
+import BackgroundOpacity from "../components/BackgroundOpacity";
+import SearchButton from "../components/SearchButton";
+import axios from "axios";
+
+export const ChangePassword = (props) => {
+  const [userInput, setUserInput] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8000/api/auth/login", userInput)
+      .then((res) => {
+        alert(res.data);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  return (
+    <>
+      <HeaderAccount />
+      <BackgroundOpacity />
+
+      <div className={styles.MainDiv}>
+        <form onSubmit={handleSubmit}>
+          <input
+            className={styles.EmailBox}
+            type="email"
+            name="email"
+            value={userInput.email}
+            placeholder="Email"
+            onChange={handleChange}
+          />
+          <input
+            className={styles.PasswordBox}
+            type="password"
+            name="password"
+            value={userInput.password}
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          <div className={styles.AccountButton}>
+            <AccountButton
+              text={"Login"}
+              linkTo={"/SearchPage"}
+              onClick={handleSubmit}
+            />
+          </div>
+        </form>
+
+        <ChangeButton text={"Change Password?"} linkTo={"/register"} />
+
+        <ChangeButton
+          text={"Don't have an account? Register here."}
+          linkTo={"/register"}
+        />
+      </div>
+    </>
+  );
+};
