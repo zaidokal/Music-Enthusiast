f49d1223512bb97b769b2e9f12099a2102693dc3
Zaid Okal <zokal@uwo.ca>
2022-12-07 19:58:58 -0500

    ListCard Design Setup

--- client/src/components/ListCard.jsx
+++ client/src/components/ListCard.jsx
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import styles from "./AccountButton.module.css";
+import axios from "axios";
+import styles from "./ListCard.module.css";
 import { Link } from "react-router-dom";
 
 const ListCard = (props) => {
+  let playList = props.playList;
 
-    let playList = props.playList;
-
-    return(
-        <Link to={`/tracks/${playList.listName}`}>
-            <div>
-                {playList.listName}
-                {playList.creator}
-                {playList.tracks}
-                {playList.playtime}
-                {playList.average_rating}
-            </div>
-        </Link>
-    )
-}
+  return (
+    <Link to={`/tracks/${playList.listName}`}>
+      <button className={styles.PlaylistInfoButton}>
+        <div className={styles.ListName}>
+          Playlist Name: {playList.listName}
+        </div>
+        <div className={styles.Creator}>Creator: {playList.creator}</div>
+        <div className={styles.Tracks}>Tracks: {playList.tracks}</div>
+        <div className={styles.Playtime}>Playtime: {playList.playtime}</div>
+        <div className={styles.AverageRating}>
+          Average Rating: {playList.average_rating}
+        </div>
+      </button>
+    </Link>
+  );
+};
 
 export default ListCard;
